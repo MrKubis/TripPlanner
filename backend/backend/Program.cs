ea2635735c1fbb99719b3ea18262ae9060b4386b
Jakub Frączek <jakub.f102@gmail.com>
2026-02-18 13:59:22 +0100

    Feat: Adding middleware

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,9 +1,30 @@
+using backend.Data;
+using backend.Domain.Entities;
+using backend.Exceptions.Handlers;
+using backend.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Exception handlers
+
+builder.Services.AddControllers();
+
+builder.Services.AddSingleton<MongoDbService>();
+builder.Services.AddSingleton(sp =>
+{
+    var db = sp.GetRequiredService<MongoDbService>().Database;
+    return db.GetCollection<Trip>("trips");
+});
+
+builder.Services.AddScoped<TripService>();
+
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddExceptionHandler<NotFoundExceptionHandler>();
 
 var app = builder.Build();
 
@@ -14,6 +35,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseExceptionHandler(_ => { });
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
